46c27f2a4fe0ad9ed9c9175272a6a8b63638dde3
metasignal <timcollinsdesign+metasignal@gmail.com>
2022-12-17 20:42:02 -0500

    serving html files now

--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
 from app import app
 
+from flask import render_template
+
 @app.route("/")
 def index():
-  return "Hello World"
+  return render_template("public/index.html")
 
 @app.route("/about")
 def about():
