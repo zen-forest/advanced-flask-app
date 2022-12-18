38a70b7593064d2a157b60d2069fa580fa2282cd
metasignal <timcollinsdesign+metasignal@gmail.com>
2022-12-17 20:03:33 -0500

    added about view

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
   return "Hello World"
 
+@app.route("/about")
+def about():
+  return "<h1 style='color: red' >This is a test!!</h1>"
+
 if __name__ == "__main__":
   app.run()
